1203bb127981374fae23c3158ca62f34b9995af6
Alejandro González González <agongon@arte-consultores.com>
2025-07-05 11:07:39 +0100

    Probando contenido proyectado

--- 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
+++ 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -1,5 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorButtonComponent } from './calculator-button.component';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CalculatorButtonComponent],
+  template: `<calculator-button>
+               <span class="projected-content underline">Test content</span>
+             </calculator-button>`
+
+})
+class TestHostComponent {}
 
 describe('CalculatorButtonComponent', () => {
 
@@ -43,7 +54,6 @@ describe('CalculatorButtonComponent', () => {
     spyOn(component.onClick, 'emit');
     component.handleClick();
     expect(component.onClick.emit).toHaveBeenCalled();
-    //expect(component.onClick.emit).toHaveBeenCalledWith('');
   });
 
   it('should set isPresed to true and then false when keyboardPressStyle us called with a matching key', (done) => {
@@ -54,6 +64,21 @@ describe('CalculatorButtonComponent', () => {
       expect(component.isPressed()).toBeFalse();
       done();
     }, 101);
-    //expect(component.onClick.emit).toHaveBeenCalledWith('');
+  });
+
+  it('should not set isPressed to true if key is not matching', () => {
+    component.contentValue()!.nativeElement.innerText = '1';
+    component.keyboardPressedStyle('2');
+    expect(component.isPressed()).toBeFalse();
+  });
+
+  it('should display projected content', () => {
+    const testHostFixture = TestBed.createComponent(TestHostComponent);
+    const compiled = testHostFixture.nativeElement as HTMLDivElement;
+    console.log(compiled);
+    expect(compiled.querySelector('.projected-content')).not.toBeNull();
+    expect(compiled.querySelector('.projected-content')?.classList.contains('underline')).toBeTrue();
+
+
   });
 });
