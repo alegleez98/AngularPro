c98c007baeb1955783330b0e6925a7115ca4a538
Alejandro González González <agongon@arte-consultores.com>
2025-09-27 11:51:40 +0100

    PLATFORM_ID - Cliente y Servidor

--- 02-pokemon-ssr/src/app/pages/princing-page/pricing-page.component.ts
+++ 02-pokemon-ssr/src/app/pages/princing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -12,11 +13,17 @@ export class PricingPageComponent {
 
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
-    this.title.setTitle('Pricing Page');
-    this.meta.updateTag({ name: 'description', 'content': 'Este es mi Pricing Page' });
-    this.meta.updateTag({ name: 'og:title', 'content': 'Pricing Page' });
-    this.meta.updateTag({ name: 'keywords', 'content': 'Alejandro,Curso,Angular,Pro,PRO' });
+    if (isPlatformServer(this.platform)) {
+      this.title.setTitle('Pricing Page');
+      this.meta.updateTag({ name: 'description', 'content': 'Este es mi Pricing Page' });
+      this.meta.updateTag({ name: 'og:title', 'content': 'Pricing Page' });
+      this.meta.updateTag({ name: 'keywords', 'content': 'Alejandro,Curso,Angular,Pro,PRO' });
+    }
+    if (isPlatformBrowser(this.platform)) {
+      document.title = 'Pricing Page';
+    }
   }
 }
--- 02-pokemon-ssr/src/app/shared/components/navBar/navBar.component.ts
+++ 02-pokemon-ssr/src/app/shared/components/navBar/navBar.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navBar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navBar.component.html',
   styleUrl: './navBar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
